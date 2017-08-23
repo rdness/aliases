5d38ae04cbb56c37a3aba4316bb80394ebacd106
rdness <orderofmoti@gmail.com>
2017-08-23 00:04:22 -0700

    Parameterized hue groups

--- common/apps/hue.sh
+++ common/apps/hue.sh
@@ -12,27 +12,27 @@ export HUEBEDGROUP="2"
 function bedon
 {
 	if [[ $1 -gt 0 ]]; then
-		curl -X PUT -H "Content-type: application/json" http://$HUESERVER/api/$HUEKEY/groups/1/action -d '{ "on": true, "bri": '$1',"effect": "none"}' &> /dev/null
+		curl -X PUT -H "Content-type: application/json" http://$HUESERVER/api/$HUEKEY/groups/$HUEBEDGROUP/action -d '{ "on": true, "bri": '$1',"effect": "none"}' &> /dev/null
 	else
-		curl -X PUT -H "Content-type: application/json" http://$HUESERVER/api/$HUEKEY/groups/1/action -d '{ "on": true, "bri": 254, "effect": "none"}' &> /dev/null
+		curl -X PUT -H "Content-type: application/json" http://$HUESERVER/api/$HUEKEY/groups/$HUEBEDGROUP/action -d '{ "on": true, "bri": 254, "effect": "none"}' &> /dev/null
 	fi
 }		
 
 function bedoff
 {
-	curl -X PUT -H "Content-type: application/json" http://$HUESERVER/api/$HUEKEY/groups/1/action -d '{ "on": false, "effect": "none"}' &> /dev/null
+	curl -X PUT -H "Content-type: application/json" http://$HUESERVER/api/$HUEKEY/groups/$HUEBEDGROUP/action -d '{ "on": false, "effect": "none"}' &> /dev/null
 }		
 
 function kiton
 {
 	if [[ $1 -gt 0 ]]; then
-		curl -X PUT -H "Content-type: application/json" http://$HUESERVER/api/$HUEKEY/groups/2/action -d '{ "on": true, "bri": '$1',"effect": "none"}' &> /dev/null
+		curl -X PUT -H "Content-type: application/json" http://$HUESERVER/api/$HUEKEY/groups/$HUEKITGROUP/action -d '{ "on": true, "bri": '$1',"effect": "none"}' &> /dev/null
 	else
-		curl -X PUT -H "Content-type: application/json" http://$HUESERVER/api/$HUEKEY/groups/2/action -d '{ "on": true, "bri": 254, "effect": "none"}' &> /dev/null
+		curl -X PUT -H "Content-type: application/json" http://$HUESERVER/api/$HUEKEY/groups/$HUEKITGROUP/action -d '{ "on": true, "bri": 254, "effect": "none"}' &> /dev/null
 	fi
 }	
 
 function kitoff
 {
-	curl -X PUT -H "Content-type: application/json" http://$HUESERVER/api/$HUEKEY/groups/2/action -d '{ "on": false, "effect": "none"}' &> /dev/null
+	curl -X PUT -H "Content-type: application/json" http://$HUESERVER/api/$HUEKEY/groups/$HUEKITGROUP/action -d '{ "on": false, "effect": "none"}' &> /dev/null
 }
